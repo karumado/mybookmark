15f5a5ebac91f8de7f6177b5458b1c54dd93a23f
karumado <karumado@gmail.com>
2014-03-29 17:02:04 +0900

    p.79 appに開発、本番の設定 gitignoreにvimの~ファイルを除外

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
+
+    configure :development do
+        puts 'development mode!'
+    end
+
+    configure :production do
+        puts 'production mode!'
+    end
+
+    configure do
+        puts 'all configure!'
+    end
+
     get '/' do
+        binding.pry
         'hi'
     end
 end
