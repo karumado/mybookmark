521a98dd4128f56588fc659bccbaa497ed3107f0
karumado <karumado@gmail.com>
2014-04-12 16:12:41 +0900

    CSS適用

--- ch2/shiori/app/shiori.rb
+++ ch2/shiori/app/shiori.rb
@@ -1,7 +1,23 @@
 require 'sinatra/base'
+require 'active_record'
+require_relative 'models/bookmark'
 
 class Shiori < Sinatra::Base
+
+  set :public_folder, File.expand_path(
+        File.join(root, '..', 'public'))
+
+  configure do
+    db_path = File.expand_path(File.join(root, '..', 'db', 'sqlite.db'))
+    ActiveRecord::Base.establish_connection(
+      adapter: 'sqlite3',
+      database: db_path
+    )
+  end
+
   get '/' do
+    Bookmark.create!(url: 'http://example.com')
+    @bookmarks = Bookmark.all
     erb :index
   end
 end
