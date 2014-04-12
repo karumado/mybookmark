df4e19fd003bea263a9d3bdc16872f21055ff22b
karumado <karumado@gmail.com>
2014-04-12 17:42:56 +0900

    まとめまで一気に進めたあとでレドメに記入する。

--- ch2/shiori/app/shiori.rb
+++ ch2/shiori/app/shiori.rb
@@ -1,23 +1,46 @@
 require 'sinatra/base'
 require 'active_record'
+require 'will_paginate'
 require_relative 'models/bookmark'
+require_relative 'db'
 
 class Shiori < Sinatra::Base
+  register WillPaginate::Sinatra
 
+  # パブリックフォルダの設定
   set :public_folder, File.expand_path(
         File.join(root, '..', 'public'))
 
+  # Viewで利用する　ヘルパー関数
+  helpers do
+    def h(text)
+      Rack::Utils.escape_html(text)
+    end
+  end
+
   configure do
-    db_path = File.expand_path(File.join(root, '..', 'db', 'sqlite.db'))
-    ActiveRecord::Base.establish_connection(
-      adapter: 'sqlite3',
-      database: db_path
-    )
+    DB.connect(
+      File.expand_path(File.join(root, '..',)),ENV['RACK_ENV'])
   end
 
   get '/' do
-    Bookmark.create!(url: 'http://example.com')
-    @bookmarks = Bookmark.all
+    @bookmarks = Bookmark.order('id DESC').page(params[:page])
     erb :index
+  end
+
+  get '/new' do
+    erb :new
+  end
+
+  post '/create' do
+    begin
+      # データを保存できた場合は '/' へリダイレクトする
+      Bookmark.create!(url: params[:url])
+      redirect '/'
+    rescue ActiveRecord::RecordInvalid => e
+      # データの保存に失敗したら再度登録画面を描画する
+      @bookmark = e.record
+      erb :new
+    end
   end
 end
